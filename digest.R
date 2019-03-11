c105146ea5a4a357f2fefedc0acc67ecb5f86460
Tristan Dennis <t.dennis.1@research.gla.ac.uk>
2019-03-11 11:41:37 +0000

    updated for my needs

--- digest.R
+++ digest.R
@@ -1,65 +1,79 @@
-insilico.digest <- function (DNAseq, cut_site_5prime1, cut_site_3prime1, cut_site_5prime2="NULL",  cut_site_3prime2="NULL",  
-                               cut_site_5prime3="NULL", cut_site_3prime3="NULL", cut_site_5prime4="NULL", cut_site_3prime4="NULL", verbose = TRUE) 
-{
-    recognition_code1 <- paste(cut_site_5prime1, cut_site_3prime1, sep = "")
-    digest1 <- insilico.digest.internal(DNAseq, recognition_code1, cut_site_5prime1, cut_site_3prime1)
-    if(cut_site_5prime2 == "NULL"){RESULT <- digest1}
-    if(cut_site_5prime2 != "NULL"){
-       recognition_code2 <- paste(cut_site_5prime2, cut_site_3prime2, sep = "")
-       digest2 <- insilico.digest.internal(digest1, recognition_code2, cut_site_5prime2, cut_site_3prime2)
-       RESULT <- digest2
-    }
-    if(cut_site_5prime3 != "NULL"){
-       recognition_code3 <- paste(cut_site_5prime3, cut_site_3prime3, sep = "")
-       digest3 <- insilico.digest.internal(digest2, recognition_code3, cut_site_5prime3, cut_site_3prime3)
-       RESULT <- digest3
-    }
-    if(cut_site_5prime4 != "NULL"){
-       recognition_code4 <- paste(cut_site_5prime4, cut_site_3prime4, sep = "")
-       digest4 <- insilico.digest.internal(digest3, recognition_code4, cut_site_5prime4, cut_site_3prime4)
-       RESULT <- digest4
-    }
-    RESULT <- unlist(RESULT)
-    if(verbose == TRUE){
-      if(cut_site_5prime2 == "NULL"){
-        cat("Number of restriction sites: ", length(insilico.digest.internal(DNAseq, recognition_code1, cut_site_5prime1, cut_site_3prime1))-1, "\n", sep="")
-      } 
-      if(cut_site_5prime2 != "NULL" && cut_site_5prime3 == "NULL"){
-      cat("Number of restriction sites for the first enzyme: ", length(insilico.digest.internal(DNAseq, recognition_code1, cut_site_5prime1, cut_site_3prime1))-1, "\n", sep="")
-      cat("Number of restriction sites for the second enzyme: ", length(insilico.digest.internal(DNAseq, recognition_code2, cut_site_5prime2, cut_site_3prime2))-1, "\n", sep="")
-      dig1 <- RESULT[isMatchingStartingAt(cut_site_3prime1, RESULT)]
-      dg1 <- reverseComplement(DNAStringSet(dig1))
-      re2match <- reverseComplement(DNAStringSet(cut_site_5prime2))
-      dg2 <- dg1[isMatchingStartingAt(re2match[[1]], dg1)]
-      dig2 <- reverseComplement(dg2)
-      dig1bis <- RESULT[isMatchingStartingAt(cut_site_3prime2, RESULT)]
-      dg1bis <- reverseComplement(DNAStringSet(dig1bis))
-      re2matchbis <- reverseComplement(DNAStringSet(cut_site_5prime1))
-      dg2bis <- dg1bis[isMatchingStartingAt(re2matchbis[[1]], dg1bis)]
-      dig2bis <- reverseComplement(dg2bis)
-      cat("Number of type AB and BA fragments:", length(dig2)+length(dig2bis), "\n", sep="")
-      RE1RE1.re2match <- reverseComplement(DNAStringSet(cut_site_5prime1))
-      RE1RE1.dg2 <- dg1[isMatchingStartingAt(RE1RE1.re2match[[1]], dg1)]
-      RE1RE1.dig2 <- reverseComplement(RE1RE1.dg2)
-      cat("Number of type AA fragments:", length(RE1RE1.dig2), "\n", sep="")
-      dig3 <- RESULT[isMatchingStartingAt(cut_site_3prime2, RESULT)]
-      dg3 <- reverseComplement(DNAStringSet(dig3))
-      RE2RE2.re2match <- reverseComplement(DNAStringSet(cut_site_5prime2))
-      RE2RE2.dg3 <- dg3[isMatchingStartingAt(RE2RE2.re2match[[1]], dg3)]
-      RE2RE2.dig3 <- reverseComplement(RE2RE2.dg3)
-      cat("Number of type BB fragments:", length(RE2RE2.dig3), "\n", sep="")
-      }
-      if(cut_site_5prime3 != "NULL"){
-        cat("Number of restriction sites 1: ", length(insilico.digest.internal(DNAseq, recognition_code1, cut_site_5prime1, cut_site_3prime1))-1, "\n", sep="")
-        cat("Number of restriction sites 2: ", length(insilico.digest.internal(DNAseq, recognition_code2, cut_site_5prime2, cut_site_3prime2))-1, "\n", sep="")
-        cat("Number of restriction sites 3: ", length(insilico.digest.internal(DNAseq, recognition_code3, cut_site_5prime3, cut_site_3prime3))-1, "\n", sep="")        
-      }
-      if(cut_site_5prime4 != "NULL"){
-        cat("Number of restriction sites 1: ", length(insilico.digest.internal(DNAseq, recognition_code1, cut_site_5prime1, cut_site_3prime1))-1, "\n", sep="")
-        cat("Number of restriction sites 2: ", length(insilico.digest.internal(DNAseq, recognition_code2, cut_site_5prime2, cut_site_3prime2))-1, "\n", sep="")
-        cat("Number of restriction sites 3: ", length(insilico.digest.internal(DNAseq, recognition_code3, cut_site_5prime3, cut_site_3prime3))-1, "\n", sep="") 
-        cat("Number of restriction sites 4: ", length(insilico.digest.internal(DNAseq, recognition_code4, cut_site_5prime4, cut_site_3prime4))-1, "\n", sep="")               
-      }            
-    } 
-    return(RESULT)
-}
+############################################################
+## In silico digest 
+## adapted from arne jacobs
+## christina.faust@gmail.com
+## 10 sept 2018
+############################################################
+
+ setwd("~/Genomes/Schistosoma_mansoni")
+
+# uncomment to download packages the first time
+# source("http://bioconductor.org/biocLite.R") 
+# biocLite("ShortRead")
+
+library(SimRAD)
+library(ShortRead)
+
+## Read reference genome
+ref = ref.DNAseq("schistosoma_mansoni.PRJEA36577.WBPS11.genomic.fa", prop.contigs = 1) #replace with fasta file of whole genome
+#ref =  sim.DNAseq(size=10000000, GCfreq=0.433) # simulated genome
+
+######
+## defining enzymes for ddRAD 
+
+## PstI-HF enzyme (6 base pairs)
+pst_5p1 = "CTGCA"
+pst_3p1 = "G"
+
+#MspI enzyme
+mspI_5p2 = "C"
+mspI_3p2 = "CGG"
+
+#####
+#Simulating the digest 
+simseq_dig = insilico.digest(ref, pst_5p1, pst_3p1,
+                             mspI_5p2, mspI_3p2, verbose=TRUE)
+
+#Simulating the flanking by two different restriction enzymes as it is the case for ddRAD
+simseq_sel = adapt.select(simseq_dig, type = "AB+BA", pst_5p1, pst_3p1, mspI_5p2, mspI_3p2)
+
+#Simulating the size selection step (change min and max size accordingly, try different size ranges):
+size_simseq = size.select(simseq_sel, min.size = 0, max.size = 100000000, graph=F, verbose=TRUE)
+#size_simseq_150_600 = size.select(simseq_sel, min.size = 150, max.size = 600, graph=TRUE, verbose=TRUE)
+
+size_simseq
+
+binneddata <- hist(size_simseq@ranges@width, breaks=seq(0,10000000,by=50), xlim=c(0, 10000))
+
+bins = as.integer(binneddata$breaks)
+binsnew = bins[2:200001]
+
+selectiondf = cbind(binsnew, binneddata$counts)
+colnames(selectiondf) <- c("fragment length", "count")
+head(selectiondf)
+
+write.csv(selectiondf, 'selectiondata.csv')
+
+#bk<-hist((size_simseq_300_600@ranges@width), breaks=length(size_simseq_300_600)/20, plot=FALSE)$breaks
+#hist(width(size_simseq_300_600),border="grey75", col="grey75", breaks=bk, main="", xlab="Locus size (bp)", ylab="Number of loci", xlim=c(0, 1000), ylim=c(0, 10000))
+
+#hist(width(size_simseq_300_600),border="red", col="red", add=TRUE, breaks=bk)
+#text(mean(c(min.size, max.size)), max(hist(width(size_simseq_300_600), breaks=bk, plot=FALSE)$counts), pos=4, labels=paste(length(size_simseq_300_600), " loci between ", min.size, " and ", max.size, " bp", sep=""), col="red", cex=0.9, font=2)
+
+#writeFasta(simseq_sel, file="simseq_allfragments.fa", mode ="w")
+
+#boxplot(list(width(simseq_sel), width(size_simseq_300_600)))
+
+#hist(unselectedfraglen,
+#     main="Unselected fragments",
+#     xlim=c(0, 1000000),
+#)
+#
+#unselectedfraglen = simseq_sel@ranges@width
+#
+#
+#selectedfraglen = size_simseq_300_600@ranges@width
+#hist(selectedfraglen,
+#     xlim=c(0, 10000),
+#     breaks=100
+#)
