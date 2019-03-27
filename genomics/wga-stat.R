c6dffe5d6dd4593e4463c89fe92fa6c105d00b7b
t <tristanpwdennis@gmail.com>
2019-03-27 11:42:39 +0000

    work on loading

--- genomics/wga-stat.R
+++ genomics/wga-stat.R
@@ -1,5 +1,26 @@
+#r script for performing basic sumamry stats on schisto WGS
+#if possible, use VCFtools and then ggplot to generate your nice manhattan plots
+#adegenet doesn't tolerate large SNP sets very well, and popgenome a) won't analyse multiple chromosomes and b) is very finicky about inputs
+
+
 #install external pkgs
 install.packages("PopGenome")
+install.packages("adegenet")
+install.packages("vcfR")
+install.packages("poppr")
 
-#load libs
+#load basically all popular popgen modules
 library(PopGenome)
+library(adegenet)
+library(vcfR)
+library(poppr)
+
+#load data and convert to object
+
+#setwd to dir containing vcfs
+setwd("~/Projects/Schisto/GENOMIC/VCF/popgenome/")
+
+
+
+
+
